106e27540d341cfc29d06295eaa4a6a48142ccb1
Armin <netzverweigerer@github>
2017-01-26 01:15:04 +0100

    bugfix

--- eco-postfix-whitelist-update.sh
+++ eco-postfix-whitelist-update.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
+#
 ## Author: Patrick Baber
 ## Updated: Franz Bettag
 ## Updated even more: Armin Jenewein (@netzverweigerer on GitHub)
+#
 
-## eco whitelist credentials
+## Set credentials for eco whitelist here
 user="your eco user"
 password="your eco pw"
 
 ## Download URL for the eco whitelist
 whitelist="https://****/csa-whitelist.xml"
 
+# Path to postfix configuration directory
 postfix="/etc/postfix"
 
 ## Local path for the whitelist in xml format
@@ -18,23 +21,46 @@ whitelist="$postfix/eco-whitelist/csa-whitelist.xml"
 ## Path to postfix whitelist
 override="$postfix/eco-whitelist/rbl_override"
 
+# Prints an error message and exits gracefully
 bailout () {
   echo "ERROR: $@"
   exit 255
 }
 
-depcheck () {
-  which "$@" >/dev/null 2>&1 || bailout "$@ not found in \$PATH. Exiting."
+# Determine download program
+determine () {
+	which wget >/dev/null 2>&1 && downloader=wget && return
+	which curl >/dev/null 2>&1 && downloader=curl && return
+	which fetch >/dev/null 2>&1 && downloader=fetch && return
+	downloader=""
 }
+determine
 
-depcheck wget
-depcheck curl
+# Exit if no compatible download program was found
+if [ $downloader == "" ]; then
+  bailout "No compatible download program found. Please intall wget, curl, or fetch. Exiting."
+  exit 255
+else
+  echo "Using downloader: $downloader"
+fi
 
-wget --http-user=${user} --http-passwd=${password} --no-check-certificate -N -O ${whitelist} ${whitelist} || bailout "wget returned non-zero exit code. Exiting."
-curl --user "${user}:${password}" --insecure ${whitelist} > ${whitelist} || bailout "curl returned non-zero exit code. Exiting."
+case "$downloader" in
+  wget)
+  wget --http-user=${user} --http-passwd=${password} --no-check-certificate -N -O ${whitelist} ${whitelist} || bailout "wget returned non-zero exit code. Exiting."
+  ;;
+  curl)
+    curl --user "${user}:${password}" --insecure ${whitelist} > ${whitelist} || bailout "curl returned non-zero exit code. Exiting."
+  ;;
+  fetch)
+    echo "IMPLEMENT ME."
+    exit 1
+  ;;
+esac
 
 grep -E -oe '<IP>[^<]+</IP>' ${whitelist} | sed -e 's;<IP>;;g' -e 's;</IP>; OK;g' > ${override}
 
 postmap ${override}
+exit $?
+
 
 
